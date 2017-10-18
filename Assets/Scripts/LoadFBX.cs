be0645bce54bad7c1a5e47935ea07e83927534a8
regireed89 <regireed89@gmail.com>
2017-10-18 15:22:18 -0500

    Done!

---
+++ Assets/Scripts/LoadFBX.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LoadFBX : MonoBehaviour
+{
+
+    public string fileName = "";
+    public string FBXfilename = "";
+    public string file = "";
+    public int slashCount = 0;
+    // Use this for initialization
+    void Start()
+    {
+        Doit();
+    }
+    public void Doit()
+    {
+        for (int i = fileName.Length - 1; i > 0; i--)
+        {
+            if (fileName[i] == '/')
+            {
+                Debug.Log("hello");
+                slashCount += 1;
+                if(slashCount == 1)
+                {
+                    FBXfilename += '@';
+                    i--;
+                }
+                if(slashCount == 2)
+                {
+                    break;
+                }
+            }
+            FBXfilename += fileName[i];
+        }
+        for (int i = FBXfilename.Length - 1; i > 0; i--)
+        {
+            file += FBXfilename[i];
+            if(i == 1)
+            {
+                i--;
+                file += FBXfilename[i];
+            }
+        }
+        Debug.Log("did it");
+    }
+
+}
