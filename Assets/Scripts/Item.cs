b84fa293b7a10320d66f4d4ea2f710a1ea4f8d36
regireed89 <regireed89@gmail.com>
2017-10-19 14:12:18 -0500

    loot

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item")]
+public class Item : ScriptableObject {
+
+    public string m_name;
+    [Range(0, 1)]
+    public float m_dropChance;
+   
+}
---
+++ Assets/Scripts/LootTable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LootTable")]
+public class LootTable : ScriptableObject {
+
+    public List<GameObject> items;
+
+}
